a22a18b5107ca7e5dd4083f61722c6b0c4df6301
pswhdev <patriciaswhalley@gmail.com>
2024-02-28 14:55:05 +0100

    Fix colour misspell

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,6 @@ let colours = ['blue', 'red', 'orange', 'pink', 'green', 'purple'];
 
 //To generate the secret code (random colorpick)
 for(i=0; i < 4; i++) {
-    secretCode.push(colors[Math.floor(Math.random() * 6)]);
+    secretCode.push(colours[Math.floor(Math.random() * 6)]);
   }
 
