0985f4a695f8c9ee41f5bab1883db29b6c7e1a2e
pswhdev <patriciaswhalley@gmail.com>
2024-02-28 22:02:36 +0100

    Add function to generate newsecret code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,19 @@
 let secretCode = [];
 let colours = ['blue', 'red', 'orange', 'pink', 'green', 'purple'];
 
-//To generate the secret code (random colorpick)
-for(i=0; i < 4; i++) {
-    secretCode.push(colours[Math.floor(Math.random() * 6)]);
-  }
+//To generate the secret code (random colorpick). Note: It can add the same colour multiple times
+function generateSecretCode(){
+    for (i = 0; i < 4; i++) {
+        secretCode.push(colours[Math.floor(Math.random() * 6)]);
+      }
+    return secretCode;
+}
+
+let newCode = generateSecretCode();
+
+
+
+
+
+
 
